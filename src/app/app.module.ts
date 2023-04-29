da8da2a213ad0aac6350e1adf7f632240833305a
skanderkefi <skander.kefi@esprit.tn>
2023-04-29 13:26:42 +0100

    new template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AnalitycsComponent } from './analitycs/analitycs.component';
+import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { JobComponent } from './job/job.component';
+import { CyptoComponent } from './cypto/cypto.component';
+import { MapsComponent } from './maps/maps.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
 import { ReclamationComponent } from './reclamation/reclamation.component';
-//import { ToastrService } from 'ngx-toastr';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { CalandrierComponent } from './calandrier/calandrier.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    AnalitycsComponent,
+    AuthComponent,
     HomeComponent,
-    NavbarComponent,
-    SidebarComponent,
-    LoginComponent,
-    SignUpComponent,
+    JobComponent,
+    CyptoComponent,
+    MapsComponent,
+    CheckoutComponent,
+    ShopComponent,
     ReclamationComponent,
+    CalandrierComponent,
+    LandingComponent
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
